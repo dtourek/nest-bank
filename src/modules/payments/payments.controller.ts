5838890be1787e0d08744fdab4f4aef04b3e061b
davidtourek <dtourek@centrum.cz>
2021-10-31 21:21:33 +0100

    payments.controller.ts refactor

--- src/modules/payments/payments.controller.ts
+++ src/modules/payments/payments.controller.ts
@@ -4,62 +4,38 @@ import { CreatePaymentDto, UpdatePaymentDto } from './dto/payments.dto';
 import { ICommonAuthenticatedRequest } from '../../interfaces';
 import { either } from 'fputils';
 
+const whenError = (error: Error): void => {
+  throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+};
+
+const whenSuccess = <T>(payment: T) => payment;
+
 @Controller('payments')
 export class PaymentsController {
   constructor(private readonly paymentsService: PaymentsService) {}
 
   @Post()
   async create(@Request() { user }: ICommonAuthenticatedRequest, @Body() createPaymentDto: CreatePaymentDto) {
-    return either(
-      (error) => {
-        throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
-      },
-      (result) => result,
-      await this.paymentsService.create(user.username, createPaymentDto),
-    );
+    return either(whenError, whenSuccess, await this.paymentsService.create(user.username, createPaymentDto));
   }
 
   @Get()
   async findMany(@Request() { user }: ICommonAuthenticatedRequest) {
-    return either(
-      (error) => {
-        throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
-      },
-      (payments) => payments,
-      await this.paymentsService.findMany(user.username),
-    );
+    return either(whenError, whenSuccess, await this.paymentsService.findMany(user.username));
   }
 
   @Get(':id')
   async findOne(@Param('id') id: string, @Request() { user }: ICommonAuthenticatedRequest) {
-    return either(
-      (error) => {
-        throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
-      },
-      (result) => result,
-      await this.paymentsService.findOne(+id, user.username),
-    );
+    return either(whenError, whenSuccess, await this.paymentsService.findOne(+id, user.username));
   }
 
   @Patch(':id')
   async update(@Param('id') id: string, @Request() { user }: ICommonAuthenticatedRequest, @Body() updatePaymentDto: UpdatePaymentDto) {
-    return either(
-      (error) => {
-        throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
-      },
-      (result) => result,
-      await this.paymentsService.update(+id, user.username, updatePaymentDto),
-    );
+    return either(whenError, whenSuccess, await this.paymentsService.update(+id, user.username, updatePaymentDto));
   }
 
   @Delete(':id')
   async remove(@Param('id') id: string, @Request() { user }: ICommonAuthenticatedRequest) {
-    return either(
-      (error) => {
-        throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
-      },
-      (result) => result,
-      await this.paymentsService.remove(+id, user.username),
-    );
+    return either(whenError, whenSuccess, await this.paymentsService.remove(+id, user.username));
   }
 }
